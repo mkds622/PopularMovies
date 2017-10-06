9c6a464439aaf299fd3ac0331af8b6916dc50238
mkds622 <mkds622@gmail.com>
2017-10-06 09:33:29 +0530

    MovieDBJsonUtils implemented for parsing json and extracting the data required from the result of network call.

--- app/src/main/java/com/mkds622/android/popularmovies/Utilities/MovieDBJsonUtils.java
+++ app/src/main/java/com/mkds622/android/popularmovies/Utilities/MovieDBJsonUtils.java
@@ -1,8 +1,41 @@
 package com.mkds622.android.popularmovies.Utilities;
 
+import com.mkds622.android.popularmovies.MovieItem;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by Meetu on 03/10/17.
  */
 
 public class MovieDBJsonUtils {
+
+    public static MovieItem[] getMovieDataFromJSON(String jsonMovieString)throws JSONException {
+        final String results="results";
+        final String id="id";
+        final String poster_path="poster_path";
+        final String movieNameField="title";
+
+        JSONObject J1 = new JSONObject(jsonMovieString);
+        JSONArray J2 = J1.getJSONArray(results);
+        MovieItem[] RESULT = new MovieItem[J2.length()];
+        for (int i = 0; i < RESULT.length; ++i) {
+            J1 = J2.getJSONObject(i);
+
+            Long movie_id = Long.valueOf(J1.getString(id));
+            String img_link = J1.getString(poster_path);
+            String movie_name=J1.getString(movieNameField);
+            img_link.replace('\\', '/');
+            MovieItem temp = new MovieItem(img_link, movie_id, movie_name);
+            RESULT[i] = temp;
+
+        }
+
+        return RESULT;
+    }
+
+
+
 }
--- app/src/main/java/com/mkds622/android/popularmovies/Utilities/NetworkUtils.java
+++ app/src/main/java/com/mkds622/android/popularmovies/Utilities/NetworkUtils.java
@@ -1,8 +1,91 @@
 package com.mkds622.android.popularmovies.Utilities;
 
+import android.net.Uri;
+import android.support.compat.BuildConfig;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by Meetu on 03/10/17.
  */
 
 public class NetworkUtils {
+
+    private static final String LOG_TAG = NetworkUtils.class.getSimpleName();
+
+    private static final String BASE_URL = "https://api.themoviedb.org/3/movie/";
+
+    private static final String Api_key_param = "api_key";
+
+    private static String apikey = null;
+
+    public static URL buildUrl(String sortCategory,String apiKey){
+        apikey=apiKey;
+        String Url=BASE_URL+sortCategory;
+        Uri builturi = Uri.parse(Url).buildUpon()
+                .appendQueryParameter(Api_key_param, apikey).build();
+        URL url=null;
+
+        try {
+            url=new URL(builturi.toString());
+        }catch (MalformedURLException e){
+            e.printStackTrace();;
+        }
+        Log.v(LOG_TAG, "Built URI " + url);
+
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException{
+        HttpURLConnection urlConnection=(HttpURLConnection) url.openConnection();
+        urlConnection.setRequestMethod("GET");
+        urlConnection.connect();
+        BufferedReader reader=null;
+        try{
+        InputStream inputStream = urlConnection.getInputStream();
+        StringBuffer buffer = new StringBuffer();
+        if (inputStream == null) {
+            return null;
+        }
+        reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+            // But it does make debugging a *lot* easier if you print out the completed
+            // buffer for debugging.
+            buffer.append(line + "\n");
+        }
+
+        if (buffer.length() == 0) {
+            // Stream was empty.  No point in parsing.
+            return null;
+        }
+        String jsonMovieString = buffer.toString();
+
+        return jsonMovieString;
+        } catch (IOException e) {
+            Log.w(LOG_TAG, "Error ", e);
+            return null;
+        } finally {
+        if (urlConnection != null) {
+            urlConnection.disconnect();
+        }
+        if (reader != null) {
+            try {
+                reader.close();
+            } catch (final IOException e) {
+                Log.w(LOG_TAG, "Error closing stream", e);
+            }
+        }
+        }
+    }
+
 }
